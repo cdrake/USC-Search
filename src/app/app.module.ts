58b578a68f0bfede3db29aacb9da06e0d971b91d
Chris Drake <christopher.a.drake@gmail.com>
2020-08-26 10:04:23 -0400

    Added book viewer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import { LinkifyPipe } from './pipes/linkify.pipe';
 import { PageInfoComponent } from './components/page-info/page-info.component';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { HighlightSearchTermsPipe } from './pipes/highlight-search-terms.pipe';
+import { BookViewerComponent } from './components/book-viewer/book-viewer.component';
 
 export function initializeApp(appConfig: AppConfigService) {
   console.log('initalize app called');
@@ -47,6 +48,7 @@ export function initializeApp(appConfig: AppConfigService) {
     LinkifyPipe,
     PageInfoComponent,
     HighlightSearchTermsPipe,
+    BookViewerComponent,
   ],
   imports: [
     BrowserModule,
